5e1af2e405cfea10ace4cd6754d004f598a2e50b
Marcus Dillavou <line72@line72.net>
2020-01-15 17:41:50 -0600

    Initial commit

---
+++ assets/Configuration.js
@@ -0,0 +1,31 @@
+/* -*- Mode: rjsx -*- */
+
+/*******************************************
+ * Copyright (2018)
+ *  Marcus Dillavou <line72@line72.net>
+ *  http://line72.net
+ *
+ * Montclair:
+ *  https://github.com/line72/montclair
+ *  https://montclair.line72.net
+ *
+ * Licensed Under the GPLv3
+ *******************************************/
+
+import AvailtecParser from './AvailtecParser';
+
+class Configuration {
+    constructor() {
+        // Grand Rapids, MI
+        this.center = [42.967101, -85.671030];
+
+        this.agencies = [
+            {
+                name: 'Routes',
+                parser: new AvailtecParser('https://grandrapids.gotransitapp.com/api/no.php/InfoPoint')
+            }
+        ];
+    }
+}
+
+export default Configuration;
---
+++ config.py
@@ -0,0 +1,33 @@
+# -*- mode: python -*-
+
+import transmogrifier
+
+CONFIG = transmogrifier.Config(
+    build_dir = './build',
+    repo = 'grandrapids',
+    package_name = 'montclair-grandrapids',
+    name = 'Go Grand Rapids',
+    description = 'Real Time Tracking of the Buses for Grand Rapids, MI',
+    url = 'https://grandrapids.gotransitapp.com',
+    logo_svg = 'assets/logo.svg',
+    montclair_config = transmogrifier.MontclairConfig(
+        version = '1.4.3',
+        revision = 1,
+        title = 'Go Grand Rapids',
+        first_run_text = "Welcome to Grand Rapids, MI's Real Time Bus Tracker.<br /><br />Please select one or more routes to begin!",
+        configuration_js_file = 'assets/Configuration.js'
+    ),
+    ios_config = transmogrifier.MontclairiOSConfig(
+        version = '2.0.4',
+        revision = 1,
+        app_id = 'com.gotransitapp.grandrapids',
+        app_store_id = 'REPLACE_ME',
+        app_store_url = 'https://apps.apple.com/us/app/go-grandrapids/idREPLACE_ME'
+    ),
+    android_config = transmogrifier.MontclairAndroidConfig(
+        version = '1.0.2',
+        revision = 1,
+        app_id = 'com.gotransitapp.grandrapids',
+        play_store_url = 'https://play.google.com/store/apps/details?id=com.gotransitapp.grandrapids'
+    )
+)
